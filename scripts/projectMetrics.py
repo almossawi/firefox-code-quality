a40c093f4b15c170a396e9281e9f1dac2448fe35
almossawi <almossawi@gmail.com>
2015-11-02 10:54:11 -0800

    Tested with python 3.5

--- scripts/projectMetrics.py
+++ scripts/projectMetrics.py
@@ -4,13 +4,10 @@ import sys
 def projectMetrics(db):
     metrics = db.metric(db.metrics())
 
-    print "SumCyclomatic,CountLine,CountLineCode"
-    print metrics("SumCyclomatic","CountLine","CountLineCode")
-    #for k,v in sorted(metrics.items()):
-    #    print (k,"=",v)
+    print("SumCyclomatic,CountLine,CountLineCode")
+    print(metrics['SumCyclomatic'], ",", metrics['CountLine'], ",", metrics['CountLineCode'])
 
 if __name__ == '__main__':
-    # Open Database
     args = sys.argv
     db = understand.open(args[1])
     projectMetrics(db)
